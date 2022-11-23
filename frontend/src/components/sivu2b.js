1af71c945cc239ae183d0f6ca9bc9681a6d2435c
veemtee <55981900+veemtee@users.noreply.github.com>
2022-11-23 13:54:50 +0200

    dropdown

--- frontend/src/components/sivu2b.js
+++ frontend/src/components/sivu2b.js
@@ -14,7 +14,19 @@ const Sivu2b = (props) => {
         sarjat:0,
         toistot:0
 	})
+	/*
+	const Pena = () => {
+		state.name="Penkki", 
+		state.liikeID=1
+	}
 	
+	const readLiike = () => {
+		var e = document.getElementById("Liike");
+		feedArray = e.value.split(",");
+		setState.liikeID(feedArray[0]);
+		setState.name(feedArray[1]);
+	}
+	*/
 	const onChange = (event) => {
 		setState((state) => {
 			return {
@@ -50,16 +62,39 @@ const Sivu2b = (props) => {
 			margin:"auto"
 		}}>
 			<form onSubmit={onSubmit} className="mb-3">
-
-                <label htmlFor="liikeID" className="form-label">liikeID</label>
+			<label htmlFor="liikeID" className="form-label">liikeID</label>
 				<input type="number"
 						name="liikeID"
 						id="liikeID"
 						className="form-control"
 						value={state.liikeID}
 						onChange={onChange}/>
-
-				<label htmlFor="Name" className="form-label">Name</label>
+			
+				<label htmlFor="name" className="form-label">Valitse Liike</label>
+				<select name="name" id="name" value={state.name} onChange={onChange}>
+						<option>Penkkipunnerrus</option>
+						<option>Vinopenkki</option>
+						<option>Dippi</option>
+						<option>Pystypunnerrus</option>
+						<option>Vipunosto sivulle</option>
+						<option>Ranskalainen punnerrus</option>
+					</select>
+					<select name="name" id="name" value={state.name} onChange={onChange}>
+						<option>Maastaveto</option>
+						<option>Leuanveto</option>
+						<option>Kulmasoutu</option>
+						<option>Ylätalja</option>
+						<option>Hauiskääntö</option>
+						<option>HammerCurl</option>
+					</select>
+					<select name="name" id="name" value={state.name} onChange={onChange}>
+						<option>Takakyykky</option>
+						<option>Etukyykky</option>
+						<option>Hyvää huomenta</option>
+						<option>Pohjenousu</option>
+						<option>Tempaus</option>
+						<option>Rinnalleveto</option>
+					</select>
 				<input type="text"
 						name="name"
 						id="name"
@@ -112,3 +147,66 @@ const Sivu2b = (props) => {
 }
 
 export default Sivu2b;
+
+
+/*
+<label htmlFor="liikeID" className="form-label">liikeID</label>
+				<input type="number"
+						name="liikeID"
+						id="liikeID"
+						className="form-control"
+						value={state.liikeID}
+						onChange={onChange}/>
+
+
+<label htmlFor="liikeID" className="form-label">liikeID</label>
+				<input type="number"
+						name="liikeID"
+						id="liikeID"
+						className="form-control"
+						value={state.liikeID}
+						onChange={onChange}/>
+
+
+
+<label>ValitseLiike:</label>
+				<h2>{"state.value"}</h2>
+				<div>
+				<select value={state.name} htmlFor="liikeID" className="container" onChange={e=>setState(e.target.value)}>
+					<optgroup label='Puskevat'>
+						<option value="">1:Pena</option>
+						<option value="Dippi">2:Dippi</option>
+						<option value="Pystypunnerrus">3:Pystäri</option>
+						<option value="Vipunosto">4:Vipari</option>
+						</optgroup>
+					</select>
+					</div>
+					<div>
+					<select htmlFor="liikeID" className="container">
+						<option value="">1:Pena</option>
+						<option value="Dippi">2:Dippi</option>
+						<option value="Pystypunnerrus">3:Pystäri</option>
+						<option value="Vipunosto">4:Vipari</option>
+					</select>
+					</div>
+					<input type="text"
+						name="name"
+						id="name"
+						className="form-control"
+						value={state.name}
+						onChange={setState}/>
+						
+						<label for="myFruit">What's your favorite fruit?</label>
+				<input type="text" name="name" id="name" list="mySuggestion" />
+				<datalist id="mySuggestion">
+				<option>Apple</option>
+				<option>Banana</option>
+				<option>Blackberry</option>
+				<option>Blueberry</option>
+				<option>Lemon</option>
+				<option>Lychee</option>
+				<option>Peach</option>
+				<option>Pear</option>
+				</datalist>
+
+						*/
