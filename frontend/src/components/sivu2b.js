0f8d063bc1473c7755c36428bfb8798f03138afe
veemtee <55981900+veemtee@users.noreply.github.com>
2022-11-23 14:11:33 +0200

    sivu2bDropdown

--- frontend/src/components/sivu2b.js
+++ frontend/src/components/sivu2b.js
@@ -14,7 +14,19 @@ const Sivu2b = (props) => {
         sarjat:0,
         toistot:0
 	})
+	/*
+	const Pena = () => {
+		state.name="Penkki", 
+		state.liikeID=1
+	}
 	
+	const readLiike = () => {
+		var e = document.getElementById("Liike");
+		feedArray = e.value.split(",");
+		setState.liikeID(feedArray[0]);
+		setState.name(feedArray[1]);
+	}
+	*/
 	const onChange = (event) => {
 		setState((state) => {
 			return {
@@ -50,16 +62,39 @@ const Sivu2b = (props) => {
 			margin:"auto"
 		}}>
 			<form onSubmit={onSubmit} className="mb-3">
-
-                <label htmlFor="liikeID" className="form-label">liikeID</label>
+			<label htmlFor="liikeID" className="form-label">liikeID</label>
 				<input type="number"
 						name="liikeID"
 						id="liikeID"
 						className="form-control"
 						value={state.liikeID}
 						onChange={onChange}/>
-
-				<label htmlFor="Name" className="form-label">Name</label>
+			
+				<label htmlFor="name" className="form-label">Valitse Liike</label>
+				<select name="name" id="name" value={state.name} onChange={onChange}>
+						<option>Penkkipunnerrus</option>
+						<option>Vinopenkki</option>
+						<option>Dippi</option>
+						<option>Pystypunnerrus</option>
+						<option>Vipunosto sivulle</option>
+						<option>Ranskalainen punnerrus</option>
+					</select>
+					<select name="name" id="name" value={state.name} onChange={onChange}>
+						<option>Maastaveto</option>
+						<option>Leuanveto</option>
+						<option>Kulmasoutu</option>
+						<option>Ylätalja</option>
+						<option>Hauiskääntö</option>
+						<option>HammerCurl</option>
+					</select>
+					<select name="name" id="name" value={state.name} onChange={onChange}>
+						<option>Takakyykky</option>
+						<option>Etukyykky</option>
+						<option>Hyvää huomenta</option>
+						<option>Pohjenousu</option>
+						<option>Tempaus</option>
+						<option>Rinnalleveto</option>
+					</select>
 				<input type="text"
 						name="name"
 						id="name"
